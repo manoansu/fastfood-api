de8b9d5ba5c7f11613b5e27c321848be54348362
manoansu <manoansu@gmail.com>
2022-04-27 14:35:19 +0100

    The classes state and address are implemented!

---
+++ src/main/java/pt/amane/fastfoodapi/domain/model/Cozinha.java
@@ -0,0 +1,26 @@
+package pt.amane.fastfoodapi.domain.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+@Table(name = "cozinha")
+public class Cozinha {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private  String nome;
+}
